b8b12b88fd5692423d4d5067145fc30d41bb0d2b
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-01-31 23:31:13 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,9 @@ def my_select(collection)
 
  i = 0
    while i < collection.length
-    new_array << yield(collection[i])
+    if yield(collection[i]) == true
+      new_array << collection[i]
+    end
     i += 1
    end
   new_array
